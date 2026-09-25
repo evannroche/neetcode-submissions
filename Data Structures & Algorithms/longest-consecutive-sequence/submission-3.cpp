d690b724fca63ecf5b1707d18de4b0586686761a
evannroche <79871587+evannroche@users.noreply.github.com>
2026-09-25 17:31:59 -0500

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0) {return 0;}
+        // add numbers to hashmap
+
+        sort(nums.begin(), nums.end());
+
+        int max = 1;
+        int count = 1;
+
+        for (int i=0; i < nums.size()-1; i++) {
+            if (nums[i] != nums[i+1]) {
+                if (nums[i] == nums[i+1]-1) {
+                    count++;
+                } else {
+                    count = 1;
+                }
+            }
+
+            if (count > max) {max = count;}
+        }
+
+        return max;
+    }
+};
